3658f787354bf15cf8f0ba6168900f33c008cc35
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2023-09-19 19:20:29 +0900

    setting: sever setting,create: readme.md

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000; // 포트 번호는 필요에 따라 변경 가능
+
+// 정적 파일 (예: HTML, CSS, JavaScript)을 제공하기 위해 Express에게 "public" 디렉토리를 사용하도록 지시합니다.
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// "/" 경로에 접속하면 "index.html" 파일을 반환합니다.
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// 서버를 시작합니다.
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
